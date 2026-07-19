78437c8c216c8e8f647e5fb30b065971faa85b61
Diana <mm987416013@gmail.com>
2026-07-20 00:24:35 +0800

    Initialize React App component in App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 Rise Landing</h1>
+        <p>Welcome to your React landing page!</p>
+        <p>Edit <code>src/App.js</code> to get started.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
