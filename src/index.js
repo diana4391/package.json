8d376f3907b1460812cab2ce0eeb345ba6d62e06
Diana <mm987416013@gmail.com>
2026-08-08 00:06:27 +0800

    Rename App component and root element identifiers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./應用程式";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("根"));
 root.render(
   <React.StrictMode>
     <App />
